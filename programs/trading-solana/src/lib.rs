df46dbf68be8024ee59100ec1fdd5ff798653aa4
hulk19 <phamhungbkhn@gmail.com>
2024-07-02 18:23:04 +0700

    update: libs

--- programs/trading-solana/src/lib.rs
+++ programs/trading-solana/src/lib.rs
@@ -6,10 +6,32 @@ declare_id!("5KAthXLCxvU7qWcK2yRKN83wgvzQgL9g9zLLnXYQEw5z");
 pub mod trading_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
+        ctx.accounts.my_account.data = data;
+        Ok(())
+    }
+
+    pub fn get_data(ctx: Context<SetData>) -> Result<u64> {
+        Ok(ctx.accounts.my_account.data)
+    }
+
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+    data: u64
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
